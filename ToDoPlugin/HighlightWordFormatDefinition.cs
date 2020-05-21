e977db62da808f47d4e14b35379a58af13ffa5c7
Xsaril <ALEX.Sytsev.2014@yandex.ru>
2020-05-21 14:49:57 +0300

    implement sample plugin

---
+++ ToDoPlugin/HighlightWordFormatDefinition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using System.Windows.Media;
+using System.ComponentModel.Composition;
+
+namespace ToDoPlugin {
+
+	[Export(typeof(EditorFormatDefinition))]
+	[Name("MarkerFormatDefinition/HighlightWordFormatDefinition")]
+	[UserVisible(true)]
+	internal class HighlightWordFormatDefinition : MarkerFormatDefinition {
+
+		public HighlightWordFormatDefinition() {
+			this.BackgroundColor = Colors.DarkRed;
+			this.ForegroundColor = Colors.Red;
+			this.ZOrder = 5;
+			this.DisplayName = "test bl";
+		}
+
+
+
+	}
+}
---
+++ ToDoPlugin/HighlightWordTaggerProvider.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoPlugin {
+
+	[Export(typeof(IViewTaggerProvider))]
+	[ContentType("text")]
+	[TagType(typeof(TextMarkerTag))]
+	internal class HighlightWordTaggerProvider : IViewTaggerProvider {
+
+		[Import]
+		internal ITextSearchService TextSearchService { get; set; }
+
+		[Import]
+		internal ITextStructureNavigatorSelectorService TextStructureNavigatorSelector { get; set; }
+
+		public HighlightWordTaggerProvider() {
+
+			File.Create(@"C:\Alexandr\ya_ebal_etot_plugin2.txt");
+		}
+
+		public ITagger<T> CreateTagger<T>(ITextView textView, ITextBuffer buffer) where T : ITag {
+
+			//throw new Exception("1322");
+
+			File.Create(@"C:\Alexandr\ya_ebal_etot_plugin.txt");
+
+			if (textView.TextBuffer != buffer)
+				return null;
+
+			ITextStructureNavigator textStructureNavigator =
+				TextStructureNavigatorSelector.GetTextStructureNavigator(buffer);
+
+			//return new HighlightWordTagger(textView, buffer, TextSearchService, textStructureNavigator) as ITagger<T>;
+
+			return buffer.Properties.GetOrCreateSingletonProperty<HighlightWordTagger>(() => new HighlightWordTagger(textView, buffer, TextSearchService, textStructureNavigator)) as ITagger<T>;
+
+		}
+	}
+}
